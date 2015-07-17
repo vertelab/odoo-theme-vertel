02b3ea70a0d7e5d8b850f5fbe868d8e17e029c7a
HaojunZou <apollo_zhj@msn.com>
2015-07-17 14:00:10 +0200

    wilssons resource added; updated theme BS-Multipurpose-RUMA and theme Middle Logo

--- theme_vertel/static/src/js/theme_middle_logo.js
+++ theme_vertel/static/src/js/theme_middle_logo.js
@@ -1,21 +1,33 @@
-//(function ($) {
-//    var mainApp = {
-//        main_fun: function () {
-//            //ADD REMOVE PADDING CLASS ON SCROLL
-//            $(window).scroll(function () {
-//                if ($(".navbar").offset().top >200){
-//                    $(".navbar-inverse").addClass("navbar-pad-original");
-//                } else {
-//                    $(".navbar-inverse").removeClass("navbar-pad-original");
-//                }
-//            });
-//        },
-//        initialization: function () {
-//            mainApp.main_fun();
-//        }
-//    }
-//    // Initializing ///
-//    $(document).ready(function () {
-//        mainApp.main_fun();
-//    });
-//}(jQuery));
+(function ($) {
+    var mainApp = {
+        main_fun: function () {
+            //ADD REMOVE CLASS ON SCROLL
+            $(window).scroll(function () {
+                if ($(".navbar").offset().top >180){
+                    $("#menu-center").addClass("hidden");
+                    $("#ml-menu").addClass("ml-navbar-fixed-top");
+                    $("#ml-menu").removeClass("ml-navbar");
+                    $("#menu-leftside").removeClass("col-md-5");
+                    $("#menu-rightside").removeClass("col-md-5");
+                    $("#menu-leftside").addClass("col-md-6");
+                    $("#menu-rightside").addClass("col-md-6");
+                } else {
+                    $("#menu-center").removeClass("hidden");
+                    $("#ml-menu").removeClass("ml-navbar-fixed-top");
+                    $("#ml-menu").addClass("ml-navbar");
+                    $("#menu-leftside").removeClass("col-md-6");
+                    $("#menu-rightside").removeClass("col-md-6");
+                    $("#menu-leftside").addClass("col-md-5");
+                    $("#menu-rightside").addClass("col-md-5");
+                }
+            });
+        },
+        initialization: function () {
+            mainApp.main_fun();
+        }
+    }
+    // Initializing ///
+    $(document).ready(function () {
+        mainApp.main_fun();
+    });
+}(jQuery));
