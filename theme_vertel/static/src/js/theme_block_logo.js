8307c4c41f9ea0f9e52c7073a827d38e6fb62c33
HaojunZou <apollo_zhj@msn.com>
2015-07-20 16:54:12 +0200

    new theme "block logo" added; lillahembageri resource added

---
+++ theme_vertel/static/src/js/theme_block_logo.js
@@ -0,0 +1,23 @@
+(function ($) {
+    var mainApp = {
+        main_fun: function () {
+            //ADD REMOVE CLASS ON SCROLL
+            $(window).scroll(function () {
+                if ($(".navbar").offset().top >180){
+                    $("#bl-menu").addClass("bl-navbar-fixed-top");
+                    $("#bl-menu").removeClass("bl-navbar");
+                } else {
+                    $("#bl-menu").removeClass("bl-navbar-fixed-top");
+                    $("#bl-menu").addClass("bl-navbar");
+                }
+            });
+        },
+        initialization: function () {
+            mainApp.main_fun();
+        }
+    }
+    // Initializing ///
+    $(document).ready(function () {
+        mainApp.main_fun();
+    });
+}(jQuery));
