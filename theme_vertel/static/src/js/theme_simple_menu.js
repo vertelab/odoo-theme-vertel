c09b476761649740de06d3fa4e060c246044a465
HaojunZou <apollo_zhj@msn.com>
2015-09-17 17:34:56 +0200

    theme updated

---
+++ theme_vertel/static/src/js/theme_simple_menu.js
@@ -0,0 +1,15 @@
+$(window).scroll(function () {
+    if ($(".navbar").offset().top > margin_value){
+        $("#sm-menu").addClass("sm-navbar-fixed-top");
+        $("#sm-menu").removeClass("sm-navbar");
+        $(".navbar-header").removeClass("hidden-lg").removeClass("hidden-md").removeClass("hidden-sm");
+        $("#sm-menu-bar").removeClass("col-md-12");
+        $("#sm-menu-bar").addClass("col-md-11").addClass("navbar-right");
+    } else {
+        $("#sm-menu").removeClass("sm-navbar-fixed-top");
+        $("#sm-menu").addClass("sm-navbar");
+        $(".navbar-header").addClass("hidden-lg").addClass("hidden-md").addClass("hidden-sm");
+        $("#sm-menu-bar").removeClass("col-md-11").removeClass("navbar-right");
+        $("#sm-menu-bar").addClass("col-md-12");
+    }
+});
