67be4c69bc8987860584f92020f2af6b5b821ac3
HaojunZou <apollo_zhj@msn.com>
2015-09-21 15:28:31 +0200

    fixed bug in js of some theme that edit buttons can see in edit mod

--- theme_block_logo/static/src/js/theme_block_logo_II.js
+++ theme_block_logo/static/src/js/theme_block_logo_II.js
@@ -4,7 +4,6 @@ $(window).scroll(function () {
         $("#bl2-menu").removeClass("bl2-navbar");
         $("#top_menu").removeClass("nav-justified");
         $("#top_menu").addClass("navbar-nav").addClass("navbar-right");
-        $(".navbar-header").removeClass("hidden-lg").removeClass("hidden-md").removeClass("hidden-sm");
         $(".logo").removeClass("hidden-lg").removeClass("hidden-md").removeClass("hidden-sm");
         $("#bl2-menu-bar").removeClass("col-md-12");
         $("#bl2-menu-bar").addClass("col-md-11").addClass("navbar-right");
@@ -13,7 +12,6 @@ $(window).scroll(function () {
         $("#bl2-menu").addClass("bl2-navbar");
         $("#top_menu").removeClass("navbar-nav").removeClass("navbar-right");
         $("#top_menu").addClass("nav-justified");
-        $(".navbar-header").addClass("hidden-lg").addClass("hidden-md").addClass("hidden-sm");
         $(".logo").addClass("hidden-lg").addClass("hidden-md").addClass("hidden-sm");
         $("#bl2-menu-bar").removeClass("col-md-11").removeClass("navbar-right");
         $("#bl2-menu-bar").addClass("col-md-12");
