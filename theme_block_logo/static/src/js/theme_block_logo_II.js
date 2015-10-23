276df8440393de0d1572185fb09daaa929b6e5ed
HaojunZou <apollo_zhj@msn.com>
2015-10-24 00:48:28 +0200

    added top_menu dropdown css

--- theme_block_logo/static/src/js/theme_block_logo_II.js
+++ theme_block_logo/static/src/js/theme_block_logo_II.js
@@ -2,16 +2,16 @@ $(window).scroll(function () {
     if ($(window).scrollTop() > margin_value){
         $("#bl2-menu").addClass("bl2-navbar-fixed-top");
         $("#bl2-menu").removeClass("bl2-navbar");
-        $("#top_menu").removeClass("nav-justified");
-        $("#top_menu").addClass("navbar-nav").addClass("navbar-right");
+
+
         $(".logo").removeClass("hidden-lg").removeClass("hidden-md").removeClass("hidden-sm");
         $("#bl2-menu-bar").removeClass("col-md-12");
         $("#bl2-menu-bar").addClass("col-md-11").addClass("navbar-right");
     } else {
         $("#bl2-menu").removeClass("bl2-navbar-fixed-top");
         $("#bl2-menu").addClass("bl2-navbar");
-        $("#top_menu").removeClass("navbar-nav").removeClass("navbar-right");
-        $("#top_menu").addClass("nav-justified");
+
+
         $(".logo").addClass("hidden-lg").addClass("hidden-md").addClass("hidden-sm");
         $("#bl2-menu-bar").removeClass("col-md-11").removeClass("navbar-right");
         $("#bl2-menu-bar").addClass("col-md-12");
