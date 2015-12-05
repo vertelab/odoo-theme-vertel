069b7004a701b423148440bae7e2abdc950d2c28
HaojunZou <apollo_zhj@msn.com>
2015-12-05 12:55:14 +0100

    middle_logo theme: style changed

--- theme_middle_logo/static/src/js/theme_middle_logo.js
+++ theme_middle_logo/static/src/js/theme_middle_logo.js
@@ -1,19 +1,13 @@
 $(window).scroll(function () {
     if ($(window).scrollTop() > margin_value){
-        $("#menu-center").addClass("hidden");
         $("#ml-menu").addClass("ml-navbar-fixed-top");
         $("#ml-menu").removeClass("ml-navbar");
-        $("#menu-leftside").removeClass("col-md-5");
-        $("#menu-rightside").removeClass("col-md-5");
-        $("#menu-leftside").addClass("col-md-6");
-        $("#menu-rightside").addClass("col-md-6");
+        $("#ml-logo").removeClass("logo_grown");
+        $("#ml-logo").addClass("logo_shrink");
     } else {
-        $("#menu-center").removeClass("hidden");
         $("#ml-menu").removeClass("ml-navbar-fixed-top");
         $("#ml-menu").addClass("ml-navbar");
-        $("#menu-leftside").removeClass("col-md-6");
-        $("#menu-rightside").removeClass("col-md-6");
-        $("#menu-leftside").addClass("col-md-5");
-        $("#menu-rightside").addClass("col-md-5");
+        $("#ml-logo").removeClass("logo_shrink");
+        $("#ml-logo").addClass("logo_grown");
     }
 });
