c1350d50ad910db1d4c81b5907bf4a37d82e4463
Haojun Zou <apollo_zhj@msn.com>
2016-02-15 09:03:40 +0100

    Update news_block.js

--- snippet_addons/static/src/js/news_block.js
+++ snippet_addons/static/src/js/news_block.js
@@ -4,13 +4,10 @@ $(document).ready(function() {
     var website = openerp.website;
     var _t = openerp._t;
 
-    console.log("hello");
 //    console.log(website.session.model('blog.post').call('name_search', [], { context: website.get_context() }));
     $(".news-block").append(
         website.session.model('blog.post')
             .call('name_search', [], { context: website.get_context() })
     );
-
-    console.log("Hejsan");
 
 });
