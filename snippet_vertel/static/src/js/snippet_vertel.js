f00ba0e0bfa5950a6a76e2256be1e5a332f7900a
Haojun Zou <apollo_zhj@msn.com>
2017-01-26 13:49:24 +0100

    snippet_vertel: update js

--- snippet_vertel/static/src/js/snippet_vertel.js
+++ snippet_vertel/static/src/js/snippet_vertel.js
@@ -1,7 +1,7 @@
 $(window).scroll(function () {
     if ($(window).scrollTop() > 400){
-        $("top-side-nav").fadeIn("slow");
+        $(".top-side-nav").fadeIn("slow");
     } else {
-        $("top-side-nav").fadeOut("fast");
+        $(".top-side-nav").fadeOut("fast");
     }
 });
