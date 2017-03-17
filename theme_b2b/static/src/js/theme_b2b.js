9d9f1327b620d8e9db8a489afa33b48cdd00a1d5
Haojun Zou <apollo_zhj@msn.com>
2017-03-17 10:23:26 +0100

    theme_b2b: font shows in demo page

--- theme_b2b/static/src/js/theme_b2b.js
+++ theme_b2b/static/src/js/theme_b2b.js
@@ -19,4 +19,26 @@ $(document).ready(function() {
     $("#top_menu > .dropdown").mouseout(function() {
         $(this).removeClass("open");
     });
+    $("#fonts").find("h1").each(function() {
+        $(this).html("H1 (" + $(this).css("font-family") + ")");
+    });
+    $("#fonts").find("h2").each(function() {
+        $(this).html("H2 (" + $(this).css("font-family") + ")");
+    });
+    $("#fonts").find("h3").each(function() {
+        $(this).html("H3 (" + $(this).css("font-family") + ")");
+    });
+    $("#fonts").find("h4").each(function() {
+        $(this).html("H4 (" + $(this).css("font-family") + ")");
+    });
+    $("#fonts").find("h5").each(function() {
+        $(this).html("H5 (" + $(this).css("font-family") + ")");
+    });
+    $("#fonts").find("h6").each(function() {
+        $(this).html("H6 (" + $(this).css("font-family") + ")");
+    });
+    $("#fonts").find("p").each(function() {
+        if ($(this).html() !== "")
+            $(this).append(" (" + $(this).css("font-family") + ")");
+    });
 });
